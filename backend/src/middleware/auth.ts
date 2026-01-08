9ce229b7a50ac91009f6da954b843327f2106d31
daichan24 <156495127+daichan24@users.noreply.github.com>
2026-01-08 16:45:27 +0900

    Update auth.ts

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,5 +1,4 @@
-// @ts-nocheck
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { Role } from '@prisma/client';
 
